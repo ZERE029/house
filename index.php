cd94dc1898dd239c138e97210d6f5a39c603a796
ZERE029 <matheusvital029@gmail.com>
2023-03-17 22:11:07 -0300

    login

--- index.php
+++ index.php
@@ -4,11 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="cub.css">
+    <title>pagina1</title>
 </head>
 <body>
-    <?php
-        
-    ?>
+    <div class="container">
+        <div class="card text-bg-light mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                    <h1>Faça seu login</h1>
+                    <form action="pagina.php" method= "get">
+                        <label>Login:</label>
+                        <input type="text" name="login"/><br/><br/>
+                        <label>Email:</label>
+                        <input type="text" name="email"/><br/><br/>
+                        <input class="btn btn-success" type="submit" value="entrar" >
+                        <input class="btn btn-danger" type="reset" value="limpar">
+                    </form>
+                </div>
+        </div>
+    </div>
 </body>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </html>
