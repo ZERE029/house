3ca021344413b2c0d539d218d8cb0befb4364790
ZERE029 <matheusvital029@gmail.com>
2024-03-11 20:22:06 -0300

    att

--- timao.js
+++ timao.js
@@ -1,4 +1,11 @@
 const chk = document.getElementById('chk')
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark')
-})
+var corDestaque = getComputedStyle(document.documentElement).getPropertyValue('--cor-destaque');
+const ctk = document.getElementById('ctk')
+chk.addEventListener('click',clicar)
+function clicar(){
+    document.body.style.backgroundColor = "black"
+}
+ctk.addEventListener('click',clicou)
+function clicou(){
+    document.body.style.backgroundColor = "white"
+}
